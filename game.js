1d6a356536e6f900e931363cbf39ab185927f380
Melina Garza <mjg2290@columbia.edu>
2024-01-25 23:25:40 -0500

    make the note elements move down the columns in sequence

--- game.js
+++ game.js
@@ -34,7 +34,8 @@ const movingElement = {
     y: 0,  // initial y position
     width: 30,
     height: 30,
-    speed: 2 // pixels per frame
+    speed: 2, // pixels per frame
+    column: 0 // Initial column
 };
 
 let animationId;
@@ -61,15 +62,19 @@ function draw() {
     // Draw background columns
     drawBackgroundColumns();
 
-    // Draw the moving element
-    ctx.fillStyle = '#f59da1'; // Blue color
-    ctx.fillRect(movingElement.x, movingElement.y, movingElement.width, movingElement.height);
+    // Draw the moving element, center it in current column
+    const columnWidth = canvas.width * columnWidthPercentage * 0.65;
+    const columnX = (canvas.width - columnWidth * numColumns) / 2 + movingElement.column * columnWidth;
+    const movingElementX = columnX + (columnWidth - movingElement.width) / 2;
+
+    ctx.fillStyle = '#f59da1';
+    ctx.fillRect(movingElementX, movingElement.y, movingElement.width, movingElement.height);
 
     
     // Add text inside the rectangle
     ctx.fillStyle = '#FFF'; // White color
-    ctx.font = '16px Arial';
-    ctx.fillText(song[songIndex], movingElement.x + 5, movingElement.y + 20);
+    ctx.font = '24px Arial';
+    ctx.fillText(song[songIndex],  movingElementX + 5, movingElement.y + 20);
 
     // Update the position for the next frame
     movingElement.y += movingElement.speed;
@@ -79,13 +84,8 @@ function draw() {
         // Reset its position to the top
         movingElement.y = 0;
 
-        //move next note to the right
-        if (movingElement.x < widthMax){
-            movingElement.x += 25;
-        }
-        else{
-            movingElement.x = 50;
-        }
+        //move to next column
+        movingElement.column = (movingElement.column + 1) % numColumns;
 
         //subtract 2 points for each missed note
         let gameScore = document.getElementById('gameScore');
@@ -142,6 +142,7 @@ function restartGame(){
      //reset the blocks
      movingElement.x = 50;
      movingElement.y = 0;
+     movingElement.column = 0;
 
 
 }
@@ -165,13 +166,8 @@ document.addEventListener('keydown', function(event) {
          setTimeout(() => {
             isAnimating = true;
             movingElement.y = 0;
-            //move next note to the right
-            if (movingElement.x < widthMax){
-                movingElement.x += 25;
-            }
-            else{
-                movingElement.x = 50;
-            }
+             //move to next column
+            movingElement.column = (movingElement.column + 1) % numColumns;
        
             songIndex = (songIndex + 1) % song.length;
 
